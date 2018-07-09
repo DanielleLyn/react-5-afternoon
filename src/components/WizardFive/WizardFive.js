5b541fcd753b3b38d031099a032d28ba9b12b9ef
Danielle Cucinotta <Danielle.cucinotta@gmail.com>
2018-07-09 16:53:52 -0700

    final

--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,6 +1,8 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {connect} from 'react-redux';
+import{updateAgen} from '../../ducks/reducer';
 class WizardFive extends Component {
 
     render(){
